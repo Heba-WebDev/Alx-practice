a3af7c3d2dd59dbd12c49319cf8b28bb6369a87e
Heba Omar <jtvonline@yahoo.com>
2023-05-17 15:32:08 +0300

    testing

---
+++ test/main.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+
+/**
+ * main - entry to the program
+ *@argc: the number of arguments
+ *@argv: a pointer to an array of arguments
+ *
+ * Return: (0)
+*/
+
+
+int main(int argc, char **argv)
+{
+
+char *command = NULL, *token;
+size_t command_len = 0;
+
+while(1)
+{
+
+printf("$ ");
+fflush(stdout);
+
+}
+
+
+return (0);
+}

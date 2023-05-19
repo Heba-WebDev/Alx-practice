33b00ff850cc8100d3648e52329f27ea22d9f9f2
Heba Omar <jtvonline@yahoo.com>
2023-05-20 02:20:59 +0300

    testing

--- test/main.c
+++ test/main.c
@@ -1,5 +1,5 @@
 #include "shell.h"
-
+void process(pid_t pid, char **args, int status);
 /**
  * main - interactive shell
  *
@@ -10,7 +10,7 @@ int main(void)
 {
 char *command, *token, *args[1024];
 size_t command_len = 0;
-int i = 0, status;
+int i = 0, status = 0;
 pid_t pid;
 while (1)
 {
@@ -31,13 +31,29 @@ token = strtok(NULL, " \n");
 }
 args[i] = NULL;
 
-if (strcmp(args[0], "exit") == 0)
+if (args[0] != NULL && _strcmp(args[0], "exit") == 0)
 {
 break;
 }
 
 pid = fork();
+process(pid, args, status);
+}
+free(command);
+return (0);
+}
 
+
+/**
+ * process - creates a new process
+ *@pid: the process
+ *@args: the array of arguments
+ *@status: process id
+ * Return: void
+ */
+
+void process(pid_t pid, char **args, int status)
+{
 if (pid == -1)
 {
 perror("fork");
@@ -53,7 +69,4 @@ else
 {
 waitpid(pid, &status, 0);
 }
-}
-free(command);
-return (0);
 }
